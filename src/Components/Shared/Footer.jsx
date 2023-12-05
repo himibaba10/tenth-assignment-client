519b20594db4a7b8368b94a91e5c0ed8f97baa75
Ferdous Ahmed Himel <himibaba10@gmail.com>
2023-12-05 13:39:15 +0600

    Replaced localhost server with the vercel production server

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 import logo from "../../assets/logo.png";
 const Footer = () => {
   return (
-    <footer className="bg-primaryColor1 relative py-5 sm:py-0">
+    <footer className="bg-primaryColor1 relative py-4 sm:py-0">
       <div className="w-full h-full bg-white absolute opacity-10 top-0"></div>
       <div className="section text-white flex flex-col sm:flex-row justify-between items-center">
         <span className="flex-1">Copyright@TenthAssignment,2023</span>
